530f1b97201387e75a4b1babc403f7d13fe447eb
Captain <980097191@qq.com>
2016-05-29 17:37:17 +0800

    完成

---
+++ UEncrypt/Assets/UEncrypt/Tutorials/test.cs
@@ -0,0 +1,53 @@
+﻿/**************************
+ * 文件名:test.cs;
+ * 文件描述:客户端生成公钥 私钥，公钥发给服务器，服务器用公钥加密数据，客户端用私钥解密数据
+ * 创建日期:2016/05/29;
+ * Author:ThisisGame;
+ * Page:https://github.com/ThisisGame/UEncrypt.git
+ ***************************/
+
+using UnityEngine;
+using System.Collections;
+using System.Security.Cryptography;
+
+
+namespace ThisisGame.UEncrypt
+{
+    public class test : MonoBehaviour
+    {
+        private string privateKey = string.Empty;
+        private string publicKey = string.Empty;
+
+        void Start()
+        {
+            string some = "ThisisGame";
+            byte[] data = System.Text.Encoding.Default.GetBytes(some);
+
+            Debug.Log("src : " + some);
+
+
+            //1、客户端 -- 生成RSA私钥 公钥，并把公钥发给服务端
+            UEncrypt.RSAGenerateKey(ref privateKey, ref publicKey);
+
+            //2、服务端 -- 产生DES密钥
+            byte[] serverDesKey = new byte[] { 1, 2, 3, 4, 8, 7, 6, 5 };
+
+            //3、服务端 -- 用RSA公钥加密 DES密钥,并发给客户端
+            byte[] serverRsaEncryptDesKey= UEncrypt.RSAEncrypt(serverDesKey, publicKey);
+
+            //4、客户端 -- 用RSA私钥解密 DES密钥
+            byte[] clientRsaDecryptDesKey = UEncrypt.RSADecrypt(serverRsaEncryptDesKey, privateKey);
+
+            //5、客户端 -- 用 DES 加密网络包
+            byte[] clientDesEncryptData = UEncrypt.DESEncrypt(data, clientRsaDecryptDesKey, clientRsaDecryptDesKey);
+
+            //6、服务端 -- 用 DES 解密网络包
+            byte[] serverDesDecryptData = UEncrypt.DESDecrypt(clientDesEncryptData, serverDesKey, serverDesKey);
+
+            some = System.Text.Encoding.Default.GetString(serverDesDecryptData);
+
+            Debug.Log("decrypt : " + some);
+        }
+    }
+}
+
---
+++ UEncrypt/Assets/UEncrypt/UEncrypt/UEncrypt.cs
@@ -0,0 +1,97 @@
+﻿/**************************
+ * 文件名:UEncrypt.cs;
+ * 文件描述:RSA加密 解密;
+ * 创建日期:2016/05/29;
+ * Author:ThisisGame;
+ * Page:https://github.com/ThisisGame/UEncrypt.git
+ ***************************/
+
+
+using System.IO;
+using System.Text;
+using System.Security.Cryptography;
+
+
+namespace ThisisGame.UEncrypt
+{
+    public class UEncrypt
+    {
+        /// <summary>
+        /// 生成RSA私钥 公钥
+        /// </summary>
+        /// <param name="privateKey"></param>
+        /// <param name="publicKey"></param>
+        public static void RSAGenerateKey(ref string privateKey, ref string publicKey)
+        {
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+            privateKey = rsa.ToXmlString(true);
+            publicKey = rsa.ToXmlString(false);
+        }
+
+        /// <summary>
+        /// 用RSA公钥 加密
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="publicKey"></param>
+        /// <returns></returns>
+        public static byte[] RSAEncrypt(byte[] data, string publicKey)
+        {
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+            rsa.FromXmlString(publicKey);
+
+            byte[] encryptData = rsa.Encrypt(data, false);
+
+            return encryptData;
+        }
+
+
+        /// <summary>
+        /// 用RSA私钥 解密
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="privateKey"></param>
+        /// <returns></returns>
+        public static byte[] RSADecrypt(byte[] data, string privateKey)
+        {
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+            rsa.FromXmlString(privateKey);
+            byte[] decryptData = rsa.Decrypt(data, false);
+            return decryptData;
+        }
+
+        /// <summary>
+        /// DES加密
+        /// </summary>
+        /// <param name="data">源数据</param>
+        /// <param name="desrgbKey"></param>
+        /// <param name="desrgbIV"></param>
+        /// <returns></returns>
+        public static byte[] DESEncrypt(byte[] data,byte[] desrgbKey,byte[] desrgbIV)
+        {
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+            MemoryStream memoryStream = new MemoryStream();
+            CryptoStream cryptoStream = new CryptoStream(memoryStream, des.CreateEncryptor(desrgbKey, desrgbIV), CryptoStreamMode.Write);
+            cryptoStream.Write(data, 0, data.Length);
+            cryptoStream.FlushFinalBlock();
+            return memoryStream.ToArray();
+        }
+
+        /// <summary>
+        /// DES解密
+        /// </summary>
+        /// <param name="data">源数据</param>
+        /// <param name="desrgbKey"></param>
+        /// <param name="desrgbIV"></param>
+        /// <returns></returns>
+        public static byte[] DESDecrypt(byte[] data, byte[] desrgbKey, byte[] desrgbIV)
+        {
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+            MemoryStream memoryStream = new MemoryStream();
+            CryptoStream cryptoStream = new CryptoStream(memoryStream, des.CreateDecryptor(desrgbKey, desrgbIV), CryptoStreamMode.Write);
+            cryptoStream.Write(data, 0, data.Length);
+            cryptoStream.FlushFinalBlock();
+            return memoryStream.ToArray();
+        }
+    }
+
+}
